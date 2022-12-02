9b254b9e8e4d3d3947e3877f36b1713d5e55b071
Rohan Jacob <72426230+ro-jc@users.noreply.github.com>
2022-12-02 10:11:42 +0000

    Better error handling

--- VIT Wifi.py
+++ VIT Wifi.py
@@ -3,61 +3,86 @@ from subprocess import check_output
 from time import sleep
 
 
-vit_ssids = ['VIT5G', 'VIT2.4G']
+vit_ssids = ['VIT5G', 'VIT2.4G', 'test']
 
 login_url = "http://phc.prontonetworks.com/cgi-bin/authlogin"
 logout_url = 'http://phc.prontonetworks.com/cgi-bin/authlogout'
 
-reg_no = ''
-password = ''
+reg_no = '22BEC0602'
+password = '53jbg4r%TC'
+
+
+def login(ssid):
+    for _ in range(5):
+        try:
+            r = requests.post(login_url, data = {'userId': reg_no, 'password': password,
+                        'serviceName': 'ProntoAuthentication', 'Submit22': 'Login'}, timeout=2)
+            if r.ok:
+                print(f'You have been successfully logged in to {ssid}')
+                input()
+                return True
+        except requests.exceptions.ConnectionError:
+            sleep(0.5)
+    return False
+
+
+'''def connect():
+    for ssid in vit_ssids:
+        connection_output = check_output(f'netsh wlan connect {ssid}', shell=True).decode()
+        print(connection_output, end='')
+        
+        if login(): break
+
+    else:
+        print(f'Connection/Login failed. Try going to "{login_url}"')
+        input()'''
 
 
 check_status = check_output('netsh wlan show interface')
 check_status = check_status.decode().split('\r\n')
 
 network_status = dict()
-
 for line in check_status:
     if ':' in line:
         split = line.split(':')
         network_status[split[0].strip()] = split[1].strip()
 
+
 if 'SSID' in network_status:
     connected_ssid = network_status['SSID']
 
     if connected_ssid in vit_ssids:
-        c = 0
-        while c <= 5:
-            if requests.get(logout_url).ok:
-                print(f'You have been successfully logged out of {connected_ssid}')
-                sleep(1)
-                disconnect_output = check_output('netsh wlan disconnect').decode()
-                print(disconnect_output, end='')
+        try:
+            logged_in = requests.get('https://1.1.1.1').ok
+        
+            for _ in range(5):
+                try:
+                    if requests.get(logout_url, timeout=2).ok:
+                        print(f'You have been successfully logged out of {connected_ssid}')
+                        sleep(1)
+                        disconnect_output = check_output('netsh wlan disconnect').decode()
+                        print(disconnect_output, end='')
+                        input()
+                        break
+                except requests.exceptions.ConnectionError:
+                    sleep(0.5)
+            else:
+                print(f'Logout Failed. Try going to "{logout_url}"')
                 input()
-                break
-            sleep(0.5)
+    
+        except requests.exceptions.SSLError:
+            login(connected_ssid)
     else:
-        print(f'Logout Failed. May be connected to another wifi network. Try going to "{logout_url}" if you really want to logout')
+        print(f'You may be connected to another wifi network.')
         input()
 
 else:
     for ssid in vit_ssids:
         connection_output = check_output(f'netsh wlan connect {ssid}', shell=True).decode()
         print(connection_output, end='')
-        sleep(0.5)
-        c = 0
-        while c <= 5:
-            r = requests.post(login_url, data = {'userId': reg_no, 'password': password,
-                            'serviceName': 'ProntoAuthentication', 'Submit22': 'Login'})
-            if r.ok:
-                print(f'You have been successfully logged in to {ssid}')
-                input()
-                break
-            sleep(0.5)
-            c += 1
-        else:
-            continue
-        break
+        
+        if login(ssid): break
+
     else:
-        print(f'Login failed. Try going to "{login_url}"')
+        print(f'Connection/Login failed. Try going to "{login_url}"')
         input()
